63b75d940589a18541e334f1d5b2e31e35c4cd2f
LucasRdSilva <lucasrsilva42@gmail.com>
2021-06-21 20:26:15 -0300

    Aula 3

---
+++ Aprendendo CSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Sua idade é " + idade);
+
+
+            Console.WriteLine("Execução Finalizada. Tecle ENTER para sair. . . ");
+            Console.ReadLine();
+        }
+    }
+}
