4d5daa19ec7c1f4e352a51edb3dcde1ba0f9053b
LucasRdSilva <lucasrsilva42@gmail.com>
2021-06-22 20:28:29 -0300

    Aula 4- Variáveis de Texto

---
+++ Aprendendo CSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e Textos");
+
+            // character
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            //Tabela ASCII
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            //Concatenação
+            string titulo = "Alura Cursos de Tecnologia " + 2021;
+            string cursosProgramacao = @" - .NET
+ - Java
+ - Javascript";
+
+//@" - .NET
+//- Java
+//- Javascript";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aprendendo CSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6 - Atribuições de Variáveis");
+
+            int idade = 22;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+        }
+    }
+}
