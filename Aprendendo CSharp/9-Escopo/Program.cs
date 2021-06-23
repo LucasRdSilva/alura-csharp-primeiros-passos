bf85c0cdaa6d0193ae9f3aaeae46c946db387dd9
LucasRdSilva <lucasrsilva42@gmail.com>
2021-06-22 21:07:42 -0300

    Aula 05 - Controle de Fluxo com If

---
+++ Aprendendo CSharp/9-Escopo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Joao está acompanhado.";
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado.";
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("João pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
