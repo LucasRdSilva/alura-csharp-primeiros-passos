3d0c987bc8daeeaae25a7fa1ed18c03d5e103355
LucasRdSilva <lucasrsilva42@gmail.com>
2021-06-29 20:49:43 -0300

    Aula 06 - Controle de Fluxo com Laços de Repetição

---
+++ Aprendendo CSharp/P13-ForEncadeado/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 13");
+
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
